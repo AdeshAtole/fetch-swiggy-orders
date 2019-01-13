6ebbb7adab5d28417f846b814e1ceb2d361de052
Adesh <adeshatole200@gmail.com>
2019-01-13 16:07:47 +0530

    Run pylint

--- fetch_swiggy_orders.py
+++ fetch_swiggy_orders.py
@@ -1,11 +1,11 @@
-import requests
 import json
+import requests
 
-total_orders = -1;
-fetched_orders = 0;
-last_order_id = '';
-total_order_amt = 0;
-delivered_orders=0;
+total_orders = -1
+fetched_orders = 0
+last_order_id = ''
+total_order_amt = 0
+delivered_orders = 0
 
 cookies = {
     #swiggy cookies
@@ -16,7 +16,8 @@ headers = {
     'Accept-Encoding': 'gzip, deflate, br',
     '__fetch_req__': 'true',
     'Accept-Language': 'en-IN,en-US;q=0.9,en-GB;q=0.8,en;q=0.7',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36\
+     (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
     'Content-Type': 'application/json',
     'Accept': '*/*',
     'Cache-Control': 'no-cache',
@@ -30,24 +31,25 @@ while(total_orders == -1 or fetched_orders < total_orders-1):
         ('order_id', last_order_id),
     )
 
-    response = requests.get('https://www.swiggy.com/dapi/order/all', headers=headers, params=params, cookies=cookies)
+    response = requests.get('https://www.swiggy.com/dapi/order/all', headers=headers,
+                            params=params, cookies=cookies)
 
     data = json.loads(response.text)
     # print data;
-    if(total_orders == -1):
-        total_orders = data["data"]["total_orders"];
-    current_orders = data["data"]["orders"];
-    fetched_orders += len(current_orders);
+    if total_orders == -1:
+        total_orders = data["data"]["total_orders"]
+    current_orders = data["data"]["orders"]
+    fetched_orders += len(current_orders)
     last_order_id = current_orders[-1]["order_id"]
 
-    for order in current_orders :
-        print order["order_id"],order["order_status"]
-        if(order["order_status"] == "Delivered"):
+    for order in current_orders:
+        print order["order_id"], order["order_status"]
+        if order["order_status"] == "Delivered":
             total_order_amt += order["order_total"]
-            delivered_orders+=1
+            delivered_orders += 1
 
-    print last_order_id;
-    print fetched_orders;
+    print last_order_id
+    print fetched_orders
 
 print total_order_amt
 print delivered_orders
